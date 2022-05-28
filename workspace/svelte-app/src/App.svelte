c8a29248a99e0eb08d6463341ac220c2c7c78da7
shobaseprg <pgymlsho19128@gmail.com>
2022-05-28 20:02:47 +0900

    reset_css

--- workspace/svelte-app/src/App.svelte
+++ workspace/svelte-app/src/App.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-import Router, {push} from 'svelte-spa-router'
-import routes from "./router";
-
+  import Router, { push } from "svelte-spa-router";
+  import routes from "./router";
 </script>
 
-
-<header><button class="bg-red-300"  on:click={()=>push("/")}>home</button></header>
+<header class="bg-gray-300"><button class="bg-green-200" on:click={() => push("/")}>home</button></header>
 <main>
-<Router {routes}/>
+  <Router {routes} />
 </main>
 
 <style global lang="postcss">
-
 </style>
